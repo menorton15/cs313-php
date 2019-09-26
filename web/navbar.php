f26dab890a05cb0be023c00217416086dd3320d2
Matthew Norton <menorton15@hotmail.com>
2019-09-26 11:32:50 -0600

    added navbar.php

--- web/navbar.php
+++ web/navbar.php
@@ -1,4 +1,4 @@
-<?php
+<php?
   echo <div id="navbar">
     <div class="menu_button"><a href="/index.html">Home</a></div>
     <div class="menu_button"><a href="/assignments.html">Assignments</a></div>
