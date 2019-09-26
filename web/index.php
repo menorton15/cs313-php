b94d48eec41c30d133b86b2f2a6987bf83267170
Matthew Norton <menorton15@hotmail.com>
2019-09-26 11:52:49 -0600

    modified index page

--- web/index.php
+++ web/index.php
@@ -1,5 +1,11 @@
-<?php
+<html>
+  <body>
 
-  phpinfo();
+    <?php include 'menu.php';?>
 
-?>
+    <h1>Welcome to my home page!</h1>
+    <p>Some text.</p>
+    <p>Some more text.</p>
+
+  </body>
+</html>
